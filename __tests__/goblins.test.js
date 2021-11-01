52060608e6a68dceaf8233a8012a137177fb0407
Franco Ortega <francopdx@gmail.com>
2021-11-01 11:49:31 -0700

    add POST endpoint for Goblin; update test with request

--- __tests__/goblins.test.js
+++ __tests__/goblins.test.js
@@ -1,17 +1,25 @@
-const Goblin = require('../lib/models/Goblin')
+const fs = require('fs');
+const app = require('../lib/app');
+const request = require('supertest');
+const pool = require('../lib/utils/pool');
 
 describe('Goblin endpoint tests', () => {
-  it('creates a goblin via POST', () => {
-    const response = Goblin
-      .insert({
+  beforeEach(() => pool.query(fs.readFileSync('./sql/database.sql', 'utf-8')));
+
+  afterAll(() => pool.end());
+
+  it('creates a goblin via POST', async() => {
+    const response = await request(app)
+      .post('/api/v1/goblins')
+      .send({
         goblinName: 'Taru',
         hitPoints: 5,
         armorClass: 14,
         items: ['apple muffin', 'tiny watch on chain'],
       })
 
-    expect(response).toBeEqual({
-      id: 1,
+    expect(response.body).toEqual({
+      id: '1',
       goblinName: 'Taru',
       hitPoints: 5,
       armorClass: 14,
